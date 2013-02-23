b387668558fadc9ac126edc8a65b4a929d2de5cc
Joe Kepley <joe@blendinteractive.com>
2013-02-23 11:41:39 -0600

    Post-install changes

---
+++ ez/ezpublish_legacy/settings/siteaccess/ezdemo_site_clean_admin/site.ini.append.php
@@ -0,0 +1,41 @@
+<?php /* #?ini charset="utf-8"?
+
+[InformationCollectionSettings]
+EmailReceiver=
+
+[SiteSettings]
+SiteName=eZ Publish 5 Tutorial
+SiteURL=ez5tutorial.local
+DefaultPage=content/dashboard
+LoginPage=custom
+
+[UserSettings]
+RegistrationEmail=
+
+[SiteAccessSettings]
+RequireUserLogin=true
+RelatedSiteAccessList[]=ezdemo_site_clean_user
+RelatedSiteAccessList[]=ezdemo_site_clean_admin
+ShowHiddenNodes=true
+
+[DesignSettings]
+SiteDesign=ezdemo_site_clean_admin
+AdditionalSiteDesignList[]=admin2
+AdditionalSiteDesignList[]=admin
+AdditionalSiteDesignList[]=ezdemo
+
+[RegionalSettings]
+Locale=eng-US
+ContentObjectLocale=eng-US
+ShowUntranslatedObjects=enabled
+SiteLanguageList[]=eng-US
+TextTranslation=enabled
+
+[ContentSettings]
+CachedViewPreferences[full]=admin_navigation_content=1;admin_children_viewmode=list;admin_list_limit=1
+TranslationList=
+
+[MailSettings]
+AdminEmail=tutorial@example.com
+EmailSender=
+*/ ?>
---
+++ ez/ezpublish_legacy/settings/siteaccess/ezdemo_site_clean_user/site.ini.append.php
@@ -0,0 +1,47 @@
+<?php /* #?ini charset="utf-8"?
+
+
+[InformationCollectionSettings]
+EmailReceiver=
+
+[Session]
+SessionNamePerSiteAccess=disabled
+
+[SiteSettings]
+SiteName=eZ Publish 5 Tutorial
+SiteURL=ez5tutorial.local
+LoginPage=embedded
+AdditionalLoginFormActionURL=http://admin.ez5tutorial.local/user/login
+
+[UserSettings]
+RegistrationEmail=
+
+[SiteAccessSettings]
+RequireUserLogin=false
+RelatedSiteAccessList[]
+RelatedSiteAccessList[]=ezdemo_site_clean_user
+RelatedSiteAccessList[]=ezdemo_site_clean_admin
+ShowHiddenNodes=false
+
+[DesignSettings]
+SiteDesign=ezdemo
+AdditionalSiteDesignList[]
+AdditionalSiteDesignList[]=ezflow
+AdditionalSiteDesignList[]=base
+AdditionalSiteDesignList[]=standard
+
+[RegionalSettings]
+Locale=eng-US
+ContentObjectLocale=eng-US
+ShowUntranslatedObjects=disabled
+SiteLanguageList[]
+SiteLanguageList[]=eng-US
+TextTranslation=enabled
+
+[ContentSettings]
+TranslationList=
+
+[MailSettings]
+AdminEmail=tutorial@example.com
+EmailSender=
+*/ ?>
